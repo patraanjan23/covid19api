0a3bd5fd3e1497eda8320a60a104c655b08d5b4c
Dipanjan Patra <patraanjan23@gmail.com>
2020-04-30 01:34:34 +0530

    updated buildscript for timeseries

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "zipping files for aws beanstalk"
-zip -u zip/payload.zip .ebextensions/cron-linux.config api.key application.py key.json source.py requirements.txt
+zip -u zip/payload.zip .ebextensions/cron-linux.config api.key application.py key.json source.py requirements.txt timeseries.py
 echo "done."
